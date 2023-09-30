57d05d2a81dea36c3ffbae5e384d1eb5eca5bd77
Christian Ibarra <hola@weeks2.com>
2023-09-30 14:09:58 -0600

    added lesson controller

--- api/src/main/java/com/cardan/api/controller/LessonController.java
+++ api/src/main/java/com/cardan/api/controller/LessonController.java
@@ -1,7 +1,24 @@
 package com.cardan.api.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("lesson")
 public class LessonController {
+    @PostMapping
+    public String create() {
+        return "post";
+    }
+    @GetMapping
+    public String read(){
+        return "get";
+    }
+    @PutMapping
+    public String update(){
+        return "put";
+    }
+    @DeleteMapping
+    public String delete(){
+        return "del";
+    }
 }
