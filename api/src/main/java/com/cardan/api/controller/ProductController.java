9c6a0b898ba292cb5a58a77797417d1a6ccfdef1
Carlos Lara <cardanfx@gmail.com>
2023-09-30 15:22:02 -0600

    added request param path variable and model

--- api/src/main/java/com/cardan/api/controller/ProductController.java
+++ api/src/main/java/com/cardan/api/controller/ProductController.java
@@ -1,4 +1,6 @@
 package com.cardan.api.controller;
+import com.cardan.api.model.Product;
+import com.cardan.api.service.ProductService;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -19,6 +21,27 @@ public class ProductController {
     @DeleteMapping
     public String delete(){
         return "del";
+    }
+
+    @PostMapping("/create")
+    public Product createProduct (@RequestBody Product product){
+        return product;
+    }
+
+    @GetMapping("path/{id}")
+    public Product getProductByPathVariable(@PathVariable int id){
+        var product = new Product();
+        product.setId(id);
+        product.setQty(id*2);
+        return product;
+    }
+
+    @GetMapping("parameter")
+    public Product getProductByParameter(@RequestParam int id){
+        var product = new Product();
+        product.setId(id);
+        product.setQty(id*2);
+        return product;
     }
 }
 
--- api/src/main/java/com/cardan/api/model/Product.java
+++ api/src/main/java/com/cardan/api/model/Product.java
@@ -1,4 +1,29 @@
 package com.cardan.api.model;
 
+import lombok.Data;
+import lombok.Getter;
+
+@Data
+@Getter
 public class Product {
+
+    private int id;
+
+    private int qty;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
 }
