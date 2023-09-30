f98d9094340e75a03d69cbe322fdfa824a735c1f
Christian Ibarra <hola@weeks2.com>
2023-09-30 14:23:02 -0600

    added repository

--- api/src/main/java/com/cardan/api/respository/LessonRepository.java
+++ api/src/main/java/com/cardan/api/respository/LessonRepository.java
@@ -1,4 +1,14 @@
 package com.cardan.api.respository;
 
+import com.cardan.api.model.Lesson;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class LessonRepository {
+    public Lesson findLessonById(int id) {
+        var lesson =  new Lesson();
+        lesson.setId(id);
+        lesson.setQty(10*id);
+        return lesson;
+    }
 }
--- api/src/main/java/com/cardan/api/service/LessonService.java
+++ api/src/main/java/com/cardan/api/service/LessonService.java
@@ -1,14 +1,15 @@
 package com.cardan.api.service;
 
 import com.cardan.api.model.Lesson;
+import com.cardan.api.respository.LessonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class LessonService {
+    @Autowired
+    LessonRepository lessonRepository;
     public Lesson findById(int id) {
-        var lesson =  new Lesson();
-        lesson.setId(id);
-        lesson.setQty(10*id);
-        return null;
+        return lessonRepository.findLessonById(id);
     }
 }
