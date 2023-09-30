890ead28990c743bdaaa073dca0aed3b82d2c6d5
Carlos Lara <cardanfx@gmail.com>
2023-09-30 15:38:46 -0600

    added product repository into product service

--- api/src/main/java/com/cardan/api/respository/ProductRepository.java
+++ api/src/main/java/com/cardan/api/respository/ProductRepository.java
@@ -1,4 +1,15 @@
 package com.cardan.api.respository;
 
+import com.cardan.api.model.Product;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class ProductRepository {
+    public Product findProductById(int id){
+        var product = new Product();
+        product.setId(id);
+        product.setQty(10*id);
+        return product;
+
+    }
 }
--- api/src/main/java/com/cardan/api/service/ProductService.java
+++ api/src/main/java/com/cardan/api/service/ProductService.java
@@ -1,14 +1,15 @@
 package com.cardan.api.service;
 
 import com.cardan.api.model.Product;
+import com.cardan.api.respository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProductService {
+    @Autowired
+    ProductRepository productRepository;
     public Product findById(int id){
-        var product = new Product();
-        product.setId(id);
-        product.setQty(10*id);
-        return product;
+        return productRepository.findProductById(id);
     }
 }
