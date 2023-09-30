3f524e35e6b58720e82a54c220c9521f5bab84a2
CryptoVerseZone <cryptoverse.zone@gmail.com>
2023-09-30 14:43:09 -0600

    added product controller

--- api/src/main/java/com/cardan/api/controller/ProductController.java
+++ api/src/main/java/com/cardan/api/controller/ProductController.java
@@ -1,7 +1,25 @@
 package com.cardan.api.controller;
-
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/product")
 public class ProductController {
+    @PostMapping
+    private String create(){
+        return "post";
+    }
+    @GetMapping
+    public String read(){
+        return "get";
+    }
+    @PutMapping
+    public String update(){
+        return "put";
+    }
+    @DeleteMapping
+    public String delete(){
+        return "del";
+    }
 }
+
+
