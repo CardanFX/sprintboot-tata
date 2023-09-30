81b17a550a2ca1e2c03a3414410007da50a66418
Christian Ibarra <hola@weeks2.com>
2023-09-30 15:45:30 -0600

    wallet: added http methods

--- api/src/main/java/com/cardan/api/controller/WalletController.java
+++ api/src/main/java/com/cardan/api/controller/WalletController.java
@@ -1,7 +1,24 @@
 package com.cardan.api.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/wallet")
 public class WalletController {
+    @PostMapping
+    private String create(){
+        return "post";
+    }
+    @GetMapping
+    public String read(){
+        return "get";
+    }
+    @PutMapping
+    public String update(){
+        return "put";
+    }
+    @DeleteMapping
+    public String delete(){
+        return "del";
+    }
 }
