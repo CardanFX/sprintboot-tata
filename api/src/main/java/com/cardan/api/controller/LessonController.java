88f68e4c373b66f9a7a1f75e0eb0e687850a46c1
Christian Ibarra <hola@weeks2.com>
2023-09-30 14:17:47 -0600

    added request parameter and pathvariable

--- api/src/main/java/com/cardan/api/controller/LessonController.java
+++ api/src/main/java/com/cardan/api/controller/LessonController.java
@@ -1,5 +1,6 @@
 package com.cardan.api.controller;
 
+import com.cardan.api.model.Lesson;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -20,5 +21,23 @@ public class LessonController {
     @DeleteMapping
     public String delete(){
         return "del";
+    }
+    @PostMapping("/create")
+    public Lesson createLesson(@RequestBody Lesson lesson){
+        return lesson;
+    }
+    @GetMapping("path/{id}")
+    public Lesson getLessonByPathVariable(@PathVariable int id){
+        var lesson =  new Lesson();
+        lesson.setId(id);
+        lesson.setQty(id*2);
+        return lesson;
+    }
+    @GetMapping("/parameter")
+    public Lesson getLessonByParameter(@RequestParam int id){
+        var lesson =  new Lesson();
+        lesson.setId(id);
+        lesson.setQty(id*2);
+        return lesson;
     }
 }
